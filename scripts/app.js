cbbe5b268c041a9475456b1e0e688fb84eaf89e5
unknown <akshaya@TECH-LP-62.techaspect.com>
2016-07-14 12:16:23 +0530

    Added angular configuration

---
+++ scripts/app.js
@@ -0,0 +1,16 @@
+(function(){
+	var app = angular.module("gitSite", ['ngAnimate', 'ngRoute']).run(function($rootScope) {
+
+    });
+
+	app.config(function($routeProvider) {
+        $routeProvider.when('/home', {
+            templateUrl: 'pages/home.html'
+        });
+
+        .otherwise({
+            redirectTo: '/home'
+        });
+    });
+
+})();

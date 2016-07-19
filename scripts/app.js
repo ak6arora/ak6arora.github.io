a6e2d369b051fd57cb994518ad05270350bedcf3
unknown <akshaya@TECH-LP-62.techaspect.com>
2016-07-19 19:46:00 +0530

    header styles for mobile and desktop

--- scripts/app.js
+++ scripts/app.js
@@ -5,12 +5,29 @@
 
     app.config(function($routeProvider) {
         $routeProvider.when('/home', {
-            templateUrl: 'pages/home.html'
+            templateUrl: 'pages/home.html',
+            activetab: 'home'
         })
-
+        .when('/skills', {
+            templateUrl: 'pages/skills.html',
+            activetab: 'skills'
+        })
+        .when('/projects', {
+            templateUrl: 'pages/projects.html',
+            activetab: 'projects'
+        })
+        .when('/contact', {
+            templateUrl: 'pages/contact.html',
+            activetab: 'contact'
+        })
         .otherwise({
             redirectTo: '/home'
         });
     });
+
+    app.controller('NavigationController', ['$scope','$route', function($scope,$route){
+    	$scope.$route=$route;
+    	$scope.showNav=false;
+    }])
 
 })();

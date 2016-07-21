07c5795c163fe8843ae51c6af2ba0d4a5b3487b6
Akshay Arora <aks.7.arora@gmail.com>
2016-07-22 00:49:13 +0530

    New Logo

--- scripts/app.js
+++ scripts/app.js
@@ -1,37 +1,39 @@
 (function() {
-  var app = angular.module("gitSite", ['ngRoute','ngAnimate']).run(function($rootScope) {
-    $rootScope.enabled = true;
-  });
+    var app = angular.module("gitSite", ['ngRoute', 'ngAnimate']).run(function($rootScope) {
+        $rootScope.enabled = true;
+    });
 
-  app.config(function($routeProvider) {
-    $routeProvider.when('/home', {
-        templateUrl: 'pages/home.html',
-        activetab: 'home'
-      })
-      .when('/skills', {
-        templateUrl: 'pages/skills.html',
-        activetab: 'skills'
-      })
-      .when('/projects', {
-        templateUrl: 'pages/projects.html',
-        activetab: 'projects'
-      })
-      .when('/contact', {
-        templateUrl: 'pages/contact.html',
-        activetab: 'contact'
-      })
-      .otherwise({
-        redirectTo: '/home'
-      });
-  });
+    app.config(function($routeProvider) {
+        $routeProvider.when('/home', {
+                templateUrl: 'pages/home.html',
+                activetab: 'home'
+            })
+            .when('/skills', {
+                templateUrl: 'pages/skills.html',
+                activetab: 'skills'
+            })
+            .when('/projects', {
+                templateUrl: 'pages/projects.html',
+                activetab: 'projects'
+            })
+            .when('/contact', {
+                templateUrl: 'pages/contact.html',
+                activetab: 'contact'
+            })
+            .otherwise({
+                redirectTo: '/home'
+            });
+    });
 
-  app.controller('MainController', ['$scope', function($scope) {
-    $scope.showNav = false;
-  }])
+    app.controller('MainController', ['$scope', function($scope) {
+        $scope.showNav = false;
+    }])
 
-  app.controller('NavigationController', ['$scope', '$route', function($scope, $route) {
-    $scope.$route = $route;
-    
-  }]);
+    app.controller('NavigationController', ['$scope', '$route', function($scope, $route) {
+        $scope.$route = $route;
+        $scope.fName = "A";
+        $scope.lName = "A";
+        $scope.fullLogo=false;
+    }]);
 
 })();

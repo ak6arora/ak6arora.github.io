ca97d0255e656e6254d0c9b7dc0a3ef4cdedeff7
Akshay Arora <aks.7.arora@gmail.com>
2016-07-31 17:32:17 +0530

    loading screen

--- scripts/app.js
+++ scripts/app.js
@@ -1,45 +1,47 @@
 (function() {
-    var app = angular.module("gitSite", ['ngRoute', 'ngAnimate']).run(function($rootScope,$document) {
-        $document.ready(function(){
-            $rootScope.enabled = true;
-        })
-        
-    });
+  var app = angular.module("gitSite", ['ngRoute', 'ngAnimate']).run(function($rootScope, $document) {
+    $document.ready(function() {
+      $rootScope.enabled = true;
+    })
 
-    app.config(function($routeProvider) {
-        $routeProvider.when('/home', {
-                templateUrl: 'pages/home.html',
-                activetab: 'home'
-            })
-            .when('/skills', {
-                templateUrl: 'pages/skills.html',
-                activetab: 'skills'
-            })
-            .when('/projects', {
-                templateUrl: 'pages/projects.html',
-                activetab: 'projects'
-            })
-            .when('/contact', {
-                templateUrl: 'pages/contact.html',
-                activetab: 'contact'
-            })
-            .otherwise({
-                redirectTo: '/home'
-            });
-    });
+  });
 
-    app.controller('MainController', ['$scope', function($scope) {
-        $scope.showNav = false;
-        $scope.overlayHide = false;
-    }])
+  app.config(function($routeProvider) {
+    $routeProvider.when('/home', {
+        templateUrl: 'pages/home.html',
+        activetab: 'home'
+      })
+      .when('/skills', {
+        templateUrl: 'pages/skills.html',
+        activetab: 'skills'
+      })
+      .when('/projects', {
+        templateUrl: 'pages/projects.html',
+        activetab: 'projects'
+      })
+      .when('/contact', {
+        templateUrl: 'pages/contact.html',
+        activetab: 'contact'
+      })
+      .otherwise({
+        redirectTo: '/home'
+      });
+  });
 
-    app.controller('NavigationController', ['$scope', '$route', function($scope, $route) {
-        $scope.$route = $route;
-        $scope.fName = "A";
-        $scope.lName = "A";
-        $scope.fullLogo = false;
-    }]);
+  app.controller('MainController', ['$scope', function($scope) {
+    $scope.showNav = false;
+    $scope.overlayHide = false;
+  }])
 
+  app.controller('NavigationController', ['$scope', '$route', function($scope, $route) {
+    $scope.$route = $route;
+    $scope.fName = "A";
+    $scope.lName = "A";
+    $scope.fullLogo = false;
+  }]);
+  setTimeout(function() {
     angular.bootstrap(document, ["gitSite"]);
+  }, 2000)
+
 
 })();
